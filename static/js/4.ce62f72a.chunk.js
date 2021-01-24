bd1e262b3995cf1daaf17d48b3e90c041e99c339
Creticweb <74098049+Creticweb@users.noreply.github.com>
2021-01-24 04:42:11 +0000

    Deploying to gh-pages from  @ 43c75028a247142b8954e424ab386fe5fce7e804 🚀

---
+++ static/js/4.ce62f72a.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpapp=this.webpackJsonpapp||[]).push([[4],{67:function(t,e,n){"use strict";n.r(e);var c=n(1),a=n(16),s=n(0);e.default=function(){var t=Object(s.useState)([]),e=Object(a.a)(t,2),n=e[0],p=e[1];return Object(s.useEffect)((function(){fetch("http://dummy.restapiexample.com/api/v1/employees").then((function(t){t.json().then((function(t){console.warn("result",t),p(t.data)}))}))}),[]),Object(c.jsx)(s.Fragment,{children:n.map((function(t){return Object(c.jsx)("h1",{children:t.id})}))})}}}]);
+//# sourceMappingURL=4.ce62f72a.chunk.js.map
