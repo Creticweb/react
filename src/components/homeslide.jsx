8a94ce41d054fdd813c0df0340832960515dec9d
Creticweb <creticweb@gmail.com>
2021-01-24 20:15:20 +0530

    Components

---
+++ src/components/homeslide.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Bubble from "../img/bubble.png";
+import burger from "../img/burger1.png";
+
+ function HomeSlider ()
+        {
+          return (
+            <div>
+            <div class="home-hero-section">
+                    <div class="bubble">
+                        <img src={Bubble} alt="bubble-images " class="w-100 img-fluid" />
+                    </div>
+                  <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                           <div class="home-hero-content">
+                             <h1>Take <strong>Foo</strong> & <strong>Let’s</strong> <span>Go.</span>
+                             </h1>
+                            <p>Have any food on your mind?<span> 
+                              <a href="contact.html">Just order now!</a>
+                               </span>
+                            </p>
+                           <a class="btn menu-btn" href="#food-menu">Today's Menu <i class="icofont-double-right"></i>
+                           </a>
+                        </div>
+                     </div>
+                     <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                        <div class="burger-img">
+                           <img src={burger} class="w-100 img-fluid" alt="Burger" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            );
+        }
+ export default HomeSlider;
