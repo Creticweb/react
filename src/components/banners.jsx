d006d112394b29990702ae3aa90221ff9e290015
IamShakal <gourabdas2927@gmail.com>
2021-01-24 18:56:59 +0530

    banner

--- src/components/banners.jsx
+++ src/components/banners.jsx
@@ -3,13 +3,13 @@ import axios from "axios";
 
  function Banners (props)
      {
-       const= [banners , setBanners]=useState([]);
+       const [banners , setBanners]=useState([]);
        
        useEffect(() =>{
          getBanners();
        },[]);
        
-       const getTodos = () => {
+       const getBanners = () => {
          axios 
            .get('https://test.yarnroad.com/api/banner-list').then((response) => {setBanners(response.data);
            })
@@ -22,7 +22,28 @@ import axios from "axios";
           {banners.map ((item,index) => {
           return (
            <Suspense fallback={<div> Loading .....</div>}>
-             <h1>{item.name}</h1>
+            <div class="home-services-section">
+	<div class="container">
+		<div class="home-services">
+			<div class="form-row">
+			{banners.map ((item,index) => {
+			   return (
+				<div key="index" class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
+					<div class="image-box clearfix">
+						<div class="box-image float-left"> <img src="img/macaron.png" alt="" /> </div>
+						<div class="image-text float-left">
+							<h2>{item.name}</h2>
+							<p>{item.description}</p>
+						</div>
+					</div>
+				        );
+				     })}
+				</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
             </Suspense>
             );
             })}
