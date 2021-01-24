9dd2f8f0dd145d09969a98b2caf93e46928e234a
IamShakal <gourabdas2927@gmail.com>
2021-01-24 19:13:53 +0530

    Banners Working Check

--- src/components/banners.jsx
+++ src/components/banners.jsx
@@ -22,27 +22,14 @@ import axios from "axios";
           {banners.map ((item,index) => {
           return (
            <Suspense fallback={<div> Loading .....</div>}>
-             <div class="home-services-section">
-	<div class="container">
-		<div class="home-services">
-			<div class="form-row">
-				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
-					<div class="image-box clearfix">
-                                           {banners.map ((item,index) => {
-               return (
-						<div key ="indec"class="box-image float-left"> <img src="img/honney.png" alt="" /> </div>
-						<div class="image-text float-left">
-							<h2>{item.name}</h2>
-							<p>{item.description}</p>
-						</div>
-                                             );
+            <div key="index">
+               <ul>
+                 <li>{item.name} | {item.description}</li>
+                </ul>
+                );
             })}
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+                 
+            </div>
             </Suspense>
             );
             })}
